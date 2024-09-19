9ab037a2f861b6b218c19607cbee5582516b7189
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-09-19 20:13:14 +0900

    [jini] excel data edit in DB
    엑셀 값이 변경되면 DB에도 반영이 된다.

--- excelIntoDB.js
+++ excelIntoDB.js
@@ -4,17 +4,25 @@ import prisma from './utils/prisma.js';
 const workbook = xlsx.readFile('./player_list.xlsx');           // 엑셀 파일 읽기
 const firstSheetName = workbook.SheetNames[0];                  // 엑셀의 첫번째 시트 가져오기
 const firstShee = workbook.Sheets[firstSheetName];              // 시트 데이터 가져오기
-const firstSheeJson = xlsx.utils.sheet_to_json(firstShee);     // 해당 내용을 JSON 파일로 변환
+const firstSheeJson = xlsx.utils.sheet_to_json(firstShee);      // 해당 내용을 JSON 파일로 변환
 
 const excelPlayerId = firstSheeJson.map(item => item['id']);    // 엑셀에 있는 캐릭터 ID 모음
 
-// Player 테이블에 있는 캐릭터 ID 조회
-const playerId = await prisma.player.findMany({
+// DB 데이터를 가져온다.
+const dbData = await prisma.player.findMany({
     select: {
-        id: true
+        id: true,
+        name: true,
+        speed: true,
+        goalScoring: true,
+        shotPower: true,
+        defense: true,
+        stamina: true
     }
 });
-const dbPlayerId = playerId.map(item => item.id);
+
+// Player 테이블에 있는 캐릭터 ID 조회
+const dbPlayerId = dbData.map(item => item.id);
 
 // 엑셀에 없는 캐릭터가 DB에는 있는지 확인
 const deletePlayer = dbPlayerId.filter(player_id => !excelPlayerId.includes(player_id));
@@ -23,9 +31,7 @@ if(0 < deletePlayer.length) {
     console.log('삭제할 ID 목록: ' + deletePlayer.join(', '));
     await prisma.player.deleteMany({
         where: {
-            id: {
-                in: deletePlayer
-            }
+            id: { in: deletePlayer }
         }
     });
 }
@@ -33,26 +39,46 @@ else {
     console.log('삭제할 데이터가 없다.');
 }
 
+function isSamePlayer(player1, player2) {
+    const key = ['speed', 'goalScoring', 'shotPower', 'defense', 'stamina'];
+    return key.every(item => player1[item] === player2[item]);
+}
+
+// 새로운 데이터를 DB에 삽입 / 데이터 변경
 for(let i=0; i<firstSheeJson.length; i++) {
     const isPlayer = await prisma.player.findFirst({
         where: {
             id: firstSheeJson[i]['id']
         }
-    })
-    if(isPlayer) {
-        console.log('이미 존재하는 데이터');
-        continue;
-    }
-
-    await prisma.player.create({
-        data: {
-            id: firstSheeJson[i]['id'],
-            name: firstSheeJson[i]['name'],
-            speed: firstSheeJson[i]['speed'],
-            goalScoring: firstSheeJson[i]['goalScoring'],
-            shotPower: firstSheeJson[i]['shotPower'],
-            defense: firstSheeJson[i]['defense'],
-            stamina: firstSheeJson[i]['stamina'],
-        }
     });
+    if(isPlayer) {
+        if(!isSamePlayer(firstSheeJson[i], dbData[i])) {
+            console.log(dbData[i]['name'] + '변경!!!!');
+            await prisma.player.update({
+                data: {
+                    speed: firstSheeJson[i]['speed'],
+                    goalScoring: firstSheeJson[i]['goalScoring'],
+                    shotPower: firstSheeJson[i]['shotPower'],
+                    defense: firstSheeJson[i]['defense'],
+                    stamina: firstSheeJson[i]['stamina'],
+                },
+                where: {
+                    id: firstSheeJson[i]['id']
+                }
+            });
+        }
+    }
+    else {
+        await prisma.player.create({
+            data: {
+                id: firstSheeJson[i]['id'],
+                name: firstSheeJson[i]['name'],
+                speed: firstSheeJson[i]['speed'],
+                goalScoring: firstSheeJson[i]['goalScoring'],
+                shotPower: firstSheeJson[i]['shotPower'],
+                defense: firstSheeJson[i]['defense'],
+                stamina: firstSheeJson[i]['stamina'],
+            }
+        });
+    }
 }
