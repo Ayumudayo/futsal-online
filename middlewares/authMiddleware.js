e2256cd0ef606f245b51a377b7bcfa96f735a01d
New-mean <dmb08075@naver.com>
2024-09-20 12:40:29 +0900

    authMiddleware 생성 신민재

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';
+import prisma from '../utils/prisma.js';
+
+export default async function (req, res, next) {
+  try {
+    const { authorization } = req.header;
+
+    if (!authorization) throw new Error('토큰이 존재하지 않습니다.');
+
+    const [tokenType, token] = authorization.split(' ');
+
+    if (tokenType !== 'Bearer') throw new Error('토큰 타입이 일치하지 않습니다.');
+    const decodedToken = jwt.verify(token, 'JWT_SECRET');
+    const id = decodedToken.id;
+
+    const user = await prisma.user.findFirst({
+      where: { id: +id },
+    });
+    if (!user) {
+      res.header('authorization');
+      throw new Error('토큰 사용자가 존재하지 않습니다.');
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res.header('authorization');
+
+    switch (error.name) {
+      case 'TokenError':
+        return res.status(401).json({ message: '토큰이 만료되었습니다.' });
+      case 'JsonWebTokenError':
+        return res.status(401).json({ message: '토큰이 조작되었습니다.' });
+      default:
+        return res.status(401).json({ message: error.message ?? '비정상적인 요청입니다.' });
+    }
+  }
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { postSignup, postLogin } from '../controllers/authController.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
@@ -7,6 +8,6 @@ const router = express.Router();
 router.post('/signup', postSignup);
 
 //** 로그인 */
-router.post('/login', postLogin);
+router.post('/login', postLogin, authMiddleware);
 
 export default router;
